80b626efe370d871c560de34eb18c6118a6114df
jimmy <tliang92@gmail.com>
2023-12-20 14:37:56 +0800

    F

--- src/components/TopView/index.vue
+++ src/components/TopView/index.vue
@@ -1,18 +1,20 @@
 <script lang="ts" setup>
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import UseTopView from "../../composables/useTopView";
 
 const { init, animate } = UseTopView();
+
+const canvasWidth = ref(160);
+const canvasHeight = ref(160);
 
 onMounted(() => {
   const canvasTopView = document.getElementById("canvasball");
   const context = canvasTopView.getContext("2d");
   const ratio = window.devicePixelRatio || 1;
-  canvasTopView.width = 160 * ratio; // 实际渲染像素
-  canvasTopView.height = 160 * ratio; // 实际渲染像素
-  canvasTopView.style.width = `${160}px`; // 控制显示大小
-  canvasTopView.style.height = `${160}px`; // 控制显示大小
-
+  canvasTopView.width = canvasWidth.value * ratio; // 实际渲染像素
+  canvasTopView.height = canvasHeight.value * ratio; // 实际渲染像素
+  canvasTopView.style.width = `${canvasWidth.value}px`; // 控制显示大小
+  canvasTopView.style.height = `${canvasHeight.value}px`; // 控制显示大小
   context.scale(ratio, ratio);
 
   init(canvasTopView, context);
@@ -25,8 +27,8 @@ onMounted(() => {
     <div class="title">方位</div>
     <canvas
       id="canvasball"
-      width="160"
-      height="160"
+      :width="canvasWidth"
+      :height="canvasHeight"
       style="border: 1px solid #eeeeee"
     >
       Your browser does not support the canvas element.
