a5edc0277f6b0f24a30f8f21b64004ea26cad44f
jimmy <tliang92@gmail.com>
2023-12-21 16:27:13 +0800

    U

--- src/components/ModelView/index.vue
+++ src/components/ModelView/index.vue
@@ -4,10 +4,18 @@ const { bodys } = UseBodys();
 </script>
 
 <template>
+  <div class="title">模型</div>
   <div v-for="(body, index) in bodys" :key="index">
     <div>{{ body.name }}-{{ body.type }}-{{ body.id }}</div>
     <div :style="{ background: body.color }">{{ body.color }}</div>
   </div>
 </template>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.title {
+  font-size: 14px;
+  font-weight: 600;
+  color: #24252c;
+  margin: 20px;
+}
+</style>
